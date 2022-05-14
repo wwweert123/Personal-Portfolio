2d2921a03bb52c0b60387c85feec0c4c6305aacb
wwweert123 <puachengxuan.fp@gmail.com>
2022-05-14 21:05:06 +0800

    Made the portfolio articles using arrary
    the data array is created with the different objects that can be rendered as articles with unique IDs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,6 +5,51 @@ import IMG2 from "../../assets/react-portfolio-2.jpg";
 import IMG3 from "../../assets/react-portfolio-3.jpg";
 import IMG4 from "../../assets/react-portfolio-4.jpg";
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Project 1",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Project 2",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "Project 3",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Project 4",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: "Project 5",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+  {
+    id: 6,
+    image: IMG2,
+    title: "Project 6",
+    github: "https://github.com",
+    demo: "https://dribble.com",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section className="portfolio">
@@ -12,126 +57,27 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            /* key is needed for the map */
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              {/* the _blank would open the link in a new tab*/}
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
 
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          {/* the _blank would open the link in a new tab*/}
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-
-            <a
-              href="https://dribble.com/Alien_pixels"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
