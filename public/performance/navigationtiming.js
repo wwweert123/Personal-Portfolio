1808861b6919a594f7e6596f3a15ed080411b8b4
Frederick Pua <puachengxuan.fp@gmail.com>
2023-06-01 12:11:19 +0800

    added axios and called the post function

--- public/performance/navigationtiming.js
+++ public/performance/navigationtiming.js
@@ -1,22 +1,26 @@
+import axios from "../../api/axios";
+const PERFORMANCE_URL = "/performance";
+
+const sendVisit = async (navigations) => {
+    try {
+        const response = await axios.post(
+            PERFORMANCE_URL,
+            JSON.stringify(navigations),
+            {
+                headers: { "Content-Type": "application/json" },
+                withCredentials: true,
+            }
+        );
+        console.log(JSON.stringify(response));
+    } catch (err) {
+        if (!err?.response) {
+            console.log("No Server Response");
+        } else {
+            console.log("Registration Failed");
+        }
+    }
+};
+
 var navigations = window.performance.getEntriesByType("navigation")[0];
 console.log(navigations.toJSON());
-// try {
-//             const response = await axios.post(
-//                 "http://localhost:3500",
-//                 JSON.stringify({ user, pwd }),
-//                 {
-//                     headers: { "Content-Type": "application/json" },
-//                     withCredentials: true,
-//                 }
-//             );
-//             console.log(JSON.stringify(response));
-//         } catch (err) {
-//             if (!err?.response) {
-//                 setErrMsg("No Server Response");
-//             } else if (err.response?.status === 409) {
-//                 setErrMsg("Username Taken");
-//             } else {
-//                 setErrMsg("Registration Failed");
-//             }
-//             errRef.current.focus();
-//         }
+sendVisit(navigations);
